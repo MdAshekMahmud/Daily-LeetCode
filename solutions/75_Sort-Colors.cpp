689bedb2016761603979f5a8ae2cdce0feb01004
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-07-30 00:54:39 +0600

    Method 2: Dutch Flag Algorithm- 3 pointeer

--- solutions/75_Sort-Colors.cpp
+++ solutions/75_Sort-Colors.cpp
@@ -7,27 +7,18 @@ class Solution {
     void sortColors(vector<int> &nums) {
         int n = nums.size();
 
-        int a = 0, b = 0, c = 0;
+        int low = 0, mid = 0, high = n - 1;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0)
-                a++;
-            else if (nums[i] == 1)
-                b++;
-            else if (nums[i] == 2)
-                c++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (a > 0) {
-                nums[i] = 0;
-                a--;
-            } else if (b > 0) {
-                nums[i] = 1;
-                b--;
-            } else if (c > 0) {
-                nums[i] = 2;
-                c--;
+        while (mid <= high) {
+            if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
+            } else if (nums[mid] == 0) {
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
             }
         }
     }
