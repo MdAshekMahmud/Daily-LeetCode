189441ee8d700aa1e65b74ef8c609a157ee3aab0
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-02 23:30:15 +0600

    Rotate image solved by transposing, and then reverse each row

---
+++ solutions/48_Rotate-Image.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void rotate(vector<vector<int>> &matrix) {
+        int n = matrix.size();
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // Reverse each n
+        for (int k = 0; k < n; k++) {
+            int i = 0, j = n - 1;
+
+            while (i <= j) { // swapping k, i with k, j
+                swap(matrix[k][i], matrix[k][j]);
+                i++;
+                j--;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    s.rotate(matrix);
+
+    for (const auto &row : matrix) {
+        for (int el : row) {
+            cout << el << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
