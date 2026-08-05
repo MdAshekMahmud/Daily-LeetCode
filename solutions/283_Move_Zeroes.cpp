b0f01358a83fb5ae1bc621e1c99f2b943610c384
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-05 23:57:10 +0600

    3 possible solution

--- solutions/283_Move_Zeroes.cpp
+++ solutions/283_Move_Zeroes.cpp
@@ -1,12 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Two pointer solution
+class Solution {
+  public:
+    void moveZeroes(vector<int> &nums) {
+        int n = nums.size();
+
+        int l = 0, r = 0;
+        while (l < n && r < n) {
+            if (nums[r] == 0) {
+                r++;
+            } else {
+                swap(nums[l++], nums[r++]);
+            }
+        }
+    }
+};
+
+// Library method
+/*
 class Solution {
   public:
     void moveZeroes(vector<int> &nums) {
         stable_partition(nums.begin(), nums.end(), [](int n) { return n != 0; });
     }
 };
+*/
+
+// One pass solution
+/*
+class Solution {
+  public:
+    void moveZeroes(vector<int> &nums) {
+        int n = nums.size();
+
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[idx++], nums[i]);
+            }
+        }
+    }
+};
+*/
 
 int main() {
     Solution s;
