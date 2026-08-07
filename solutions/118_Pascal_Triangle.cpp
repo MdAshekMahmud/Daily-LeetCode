5f4117800d4d63d02f83284af08f87b7ca7fa443
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-08 00:24:40 +0600

    solved using combinatorial formula

---
+++ solutions/118_Pascal_Triangle.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for (int i = 1; i <= numRows; i++) {
+            vector<int> a(i);
+            ans.push_back(a);
+        }
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i)
+                    ans[i][j] = 1;
+                else
+                    ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
+            }
+        }
+        return ans;
+    }
+};
+int main() {
+    Solution s;
+    vector<vector<int>> matrix = s.generate(6);
+
+    for (const vector<int> &row : matrix) {
+        for (int el : row) {
+            cout << el << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
