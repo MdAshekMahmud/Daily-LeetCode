47781cbc800156e5d26144bd766ae64e7d30348e
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-07 10:50:51 +0600

    better solution O(n)

--- solutions/1572_Matrix-Diagonal-Sum.cpp
+++ solutions/1572_Matrix-Diagonal-Sum.cpp
@@ -4,22 +4,15 @@ using namespace std;
 class Solution {
   public:
     int diagonalSum(vector<vector<int>> &mat) {
-        int row = mat.size();
-        int col = mat[0].size();
+        int n = mat.size();
+        int sum = 0;
 
-        int diaSum = 0;
-        int x = col - 1;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (i == j)
-                    diaSum += mat[i][j];
-            }
-            diaSum += mat[i][x--];
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - i - 1];
         }
-        if (row % 2 != 0)
-            diaSum -= mat[row / 2][col / 2];
 
-        return diaSum;
+        return n % 2 == 0 ? sum : sum - mat[n / 2][n / 2];
     }
 };
 
