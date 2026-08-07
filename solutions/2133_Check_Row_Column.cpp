d959664f55558a8f18d52abc5ecbb4d6f0381451
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-07 12:15:29 +0600

    Check row and column using set

---
+++ solutions/2133_Check_Row_Column.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool checkValid(vector<vector<int>> &matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            set<int> st1;
+            set<int> st2;
+            for (int j = 0; j < n; j++) {
+                st1.insert(matrix[i][j]);
+            }
+            for (int j = 0; j < n; j++) {
+                st2.insert(matrix[j][i]);
+            }
+            if (st1.size() < n)
+                return false;
+            if (st2.size() < n)
+                return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> matrix = {{1, 2, 3}, {3, 2, 1}, {1, 2, 1}};
+    cout << s.checkValid(matrix) << " ";
+
+    return 0;
+}
