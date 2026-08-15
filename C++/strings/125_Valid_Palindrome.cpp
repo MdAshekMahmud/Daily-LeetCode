fa86d3056374fa7f172ad4d6cdb6e6e2fc8df258
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-15 07:14:48 +0600

    using isalnum

--- C++/strings/125_Valid_Palindrome.cpp
+++ C++/strings/125_Valid_Palindrome.cpp
@@ -5,16 +5,17 @@ class Solution {
   public:
     bool isPalindrome(string s) {
         int n = s.length();
-
+        // Convert to lower case
         for (char &c : s) {
             c = tolower(static_cast<unsigned char>(c));
         }
-
+        // Remove non alphanumeric character
         s.erase(remove_if(s.begin(), s.end(),
                           [](unsigned char c) {
                               bool is_lowercase = (c >= 97 && c <= 122);
                               bool is_digit = (c >= 48 && c <= 57);
                               return !(is_lowercase || is_digit);
+                              //   return !(ialnum(c));
                           }),
                 s.end());
 
