4f34966a2424a7bb43af58878d08785998d2c9c9
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-20 08:32:22 +0600

    using checkmark-O(1) spcae

--- C++/strings/242_Valid_Anagram.cpp
+++ C++/strings/242_Valid_Anagram.cpp
@@ -4,16 +4,22 @@ using namespace std;
 class Solution {
   public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        int len1 = s.length();
+        int len2 = t.length();
+        if (len1 != len2)
             return false;
-        }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] != t[i]) {
+        vector<int> check(26, 0);
+
+        for (char c : s) {
+            check[c - 'a']++;
+        }
+
+        for (char c : t) {
+            if (check[c - 'a'] == 0) {
                 return false;
             }
+            check[c - 'a']--;
         }
         return true;
     }
