54262dc53c4421f96c7406f08fa1e4009d39029d
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-08-22 06:57:56 +0600

    using hash map freq count

---
+++ C++/arrays/229_Majority_Element_II.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> majorityElement(vector<int> &nums) {
+        int n = nums.size();
+        map<int, int> mp;
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+
+        for (auto el : mp) {
+            if (el.second > n / 3) {
+                ans.push_back(el.first);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3, 2, 3};
+    vector<int> ans = sol.majorityElement(nums);
+
+    for (int el : ans) {
+        cout << el << " ";
+    }
+
+    return 0;
+}
