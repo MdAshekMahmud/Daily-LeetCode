3a60412135e83d566d6d6f0b9f8ab639c60a5877
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-09-14 18:55:25 +0600

    refactor containsDuplicate method to use sorting instead of set for duplicate detection

--- C++/hash_tables/217_Contains_Duplicate.cpp
+++ C++/hash_tables/217_Contains_Duplicate.cpp
@@ -4,9 +4,15 @@ using namespace std;
 class Solution {
   public:
     bool containsDuplicate(vector<int> &nums) {
-        set<int> st(nums.begin(), nums.end());
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-        return nums.size() != st.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
