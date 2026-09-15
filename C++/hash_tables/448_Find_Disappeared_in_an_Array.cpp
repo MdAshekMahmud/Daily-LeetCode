b9a6847377a960c11d19bb3ab1bc0df12e62a686
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-09-15 22:58:11 +0600

    refactor findDisappearedNumbers method to eliminate unordered_set and optimize performance

--- C++/hash_tables/448_Find_Disappeared_in_an_Array.cpp
+++ C++/hash_tables/448_Find_Disappeared_in_an_Array.cpp
@@ -5,15 +5,22 @@ class Solution {
   public:
     vector<int> findDisappearedNumbers(vector<int> &nums) {
         int n = nums.size();
-        unordered_set<int> st(nums.begin(), nums.end());
 
-        vector<int> ans;
-        for (int i = 1; i <= n; i++) {
-            if (st.find(i) == st.end()) {
-                ans.push_back(i);
+        for (int i = 0; i < n; i++) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
             }
         }
-        return ans;
+
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                res.push_back(i + 1);
+            }
+        }
+
+        return res;
     }
 };
 
