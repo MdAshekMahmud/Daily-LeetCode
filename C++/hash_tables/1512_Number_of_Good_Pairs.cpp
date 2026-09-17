5d5ac00ba46c8a32885210ac8681ca3833cea58a
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-09-18 00:07:25 +0600

    refactor numIdenticalPairs method to use unordered_map for improved performance

--- C++/hash_tables/1512_Number_of_Good_Pairs.cpp
+++ C++/hash_tables/1512_Number_of_Good_Pairs.cpp
@@ -6,17 +6,22 @@ class Solution {
     int numIdenticalPairs(vector<int> &nums) {
         int n = nums.size();
 
+        unordered_map<int, int> hash;
+        for (int i = 0; i < n; i++) {
+            hash[nums[i]]++;
+        }
+
         int cnt = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    cnt++;
-                }
+        for (auto &[x, y] : hash) {
+            if (y > 1) {
+                cnt += y * (y - 1) / 2;
             }
         }
+
         return cnt;
     }
 };
+
 int main() {
     Solution sol;
     vector<int> nums = {1, 1, 1, 1};
