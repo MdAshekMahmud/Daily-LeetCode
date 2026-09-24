04bcef647daf6c2e0bf7e8da38e4a865bcf35beb
MdAshekMahmud <ashek242-15-854@diu.edu.bd>
2026-09-24 12:18:24 +0600

    add solution for searching in a rotated sorted array

---
+++ C++/sorting_and_search/33_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int search(vector<int> &nums, int target) {
+        int n = nums.size();
+
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 0, 1, 1, 1};
+
+    cout << sol.search(nums, 0) << '\n';
+
+    return 0;
+}
